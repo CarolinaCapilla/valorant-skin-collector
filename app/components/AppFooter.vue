6999c7f180a66eb07b78a5aaec25c4e6b1537f6e
Carolina Moreno Capilla <carolina.capilla@gmail.com>
2025-10-10 18:19:55 +0200

    feat(ui): update footer/logo components

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,40 @@
+<template>
+	<!-- <USeparator icon="i-simple-icons-nuxtdotjs" type="solid" class="h-px" /> -->
+
+	<UFooter class="bg-primary">
+		<template #left>
+			<p class="text-white font-semibold">Copyright © {{ new Date().getFullYear() }}</p>
+		</template>
+
+		<template #right>
+			<UButton
+				icon="i-simple-icons-linkedin"
+				color="secondary"
+				variant="ghost"
+				to="https://go.nuxt.com/x"
+				target="_blank"
+				aria-label="X"
+				:ui="{
+					base: 'text-white'
+				}"
+				class="hover:bg-secondary-100 tracking-wide hover:shadow-md shadow-secondary-50 transition-all duration-150"
+			/>
+			<UButton
+				icon="i-simple-icons-github"
+				color="secondary"
+				variant="ghost"
+				to="https://github.com/nuxt/nuxt"
+				target="_blank"
+				aria-label="GitHub"
+				:ui="{
+					base: 'text-white'
+				}"
+				class="hover:bg-secondary-100 tracking-wide hover:shadow-md shadow-secondary-50 transition-all duration-150"
+			/>
+		</template>
+	</UFooter>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'AppFooter' })
+</script>
