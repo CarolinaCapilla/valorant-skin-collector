ce9b0cf80ac93966ef723684bb8adb90b5d5c2d2
Carolina Moreno Capilla <carolina.capilla@gmail.com>
2025-10-10 18:17:29 +0200

    chore: add project scaffolding (package.json, Tailwind config, TS config, public assets)

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+export default {
+	theme: {
+		extend: {
+			colors: {
+				valorant: {
+					red: '#FF4655', // Valorant red
+					dark: '#0F1923', // Deep background
+					gray: '#1F2A36', // Panel gray
+					light: '#ECE8E1' // Text beige
+				}
+			},
+			fontFamily: {
+				sans: ['Rajdhani', 'sans-serif'] // Valorant’s geometric font
+			}
+		}
+	}
+}
