459a4fa322715944b9e8913c8b638b5182ea0801
Carolina Moreno Capilla <carolina.capilla@gmail.com>
2025-10-10 18:19:43 +0200

    feat(ui): apply primary color to AppHeader navigation (Nuxt UI)

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,72 @@
+<template>
+	<UHeader
+		title="Valorant Skin Collector"
+		:ui="{
+			title: 'text-primary font-bold'
+		}"
+	>
+		<!-- <template #left> -->
+		<!-- <NuxtLink to="/"> -->
+		<!-- <AppLogo class="w-auto h-6 shrink-0" /> -->
+		<!-- </NuxtLink> -->
+		<!-- </template> -->
+
+		<UNavigationMenu highlight :items="items" variant="link" />
+
+		<template #right>
+			<UButton
+				icon="i-lucide-log-in"
+				color="neutral"
+				variant="ghost"
+				to="/login"
+				class="lg:hidden"
+			/>
+
+			<UButton
+				label="Sign in"
+				color="neutral"
+				variant="outline"
+				to="/login"
+				class="hidden lg:inline-flex"
+			/>
+
+			<UButton
+				label="Sign up"
+				color="neutral"
+				trailing-icon="i-lucide-arrow-right"
+				class="hidden lg:inline-flex"
+				to="/signup"
+			/>
+		</template>
+
+		<template #body>
+			<UNavigationMenu :items="items" orientation="vertical" class="-mx-2.5" />
+
+			<USeparator class="my-6" />
+
+			<UButton label="Sign in" color="neutral" variant="subtle" to="/login" block class="mb-3" />
+			<UButton label="Sign up" color="neutral" to="/signup" block />
+		</template>
+	</UHeader>
+</template>
+
+<script setup lang="ts">
+const items = computed(() => [
+	{
+		label: 'Home',
+		to: '/'
+	},
+	{
+		label: 'Collection',
+		to: '/collection'
+	},
+	{
+		label: 'Wishlist',
+		to: '/wishlist'
+	},
+	{
+		label: 'Changelog',
+		to: '/changelog'
+	}
+])
+</script>
